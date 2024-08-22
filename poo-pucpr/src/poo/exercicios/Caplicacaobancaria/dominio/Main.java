2efa66a2a90ccce46593abed3e712c24664da752
Eduardo Fabri <eduardohfabri@gmail.com>
2024-08-22 20:54:00 -0300

    Main da aplicação bancaria

---
+++ poo-pucpr/src/poo/exercicios/Caplicacaobancaria/dominio/Main.java
@@ -0,0 +1,9 @@
+package poo.exercicios.Caplicacaobancaria.dominio;
+
+public class Main {
+    public static void main(String[] args) {
+        ContaBancaria contaBancaria = new ContaBancaria();
+
+        contaBancaria.menu();
+    }
+}
